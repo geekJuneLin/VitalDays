b8dc5171efcd23c84a534e36893b3705957cba11
geekJuneLin <junelin0216@gmail.com>
2020-01-22 22:13:38 +1300

    Initial Commit

---
+++ VitalDays/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VitalDays
+//
+//  Created by Junyu Lin on 22/01/20.
+//  Copyright © 2020 Junyu Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
